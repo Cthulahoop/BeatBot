c05241dc9b7deef0bcee8c9fc4aec393e66f09b3
Cthulahoop <cthulhu@formequalsfunction.com>
2013-09-15 15:06:29 -0700

    Added more UI visual feedback, moved more logic to methods
    Added updateWave to draw UI and handle wave and envelope changes.
    Added reset of song (press + and x).
    Modified pause routine.
    Simplified big button logic.
    Changed meausre display to one that makes less noise.

--- beatbot/sequencer.h
+++ beatbot/sequencer.h
@@ -2,18 +2,26 @@
 
 byte quad;
 
-/*
- *  logic for the sequencer
+/* *************************************************************************
+ *
+ *   A 32 step sequencer for the synth
+ *
+ *   v 1.6  14.09.2013
+ *    - added comments and history
+ *    - added bmp for wave forms
+ *    - began moving more logic into methods
+ *
  */
-
 void initSequencer() {
   bar = 0;
 
   // setup_voice(voice, waveform, pitch, envelope, length,  mod)
-  setup_voice(0, (unsigned int)TriangleTable,400.0,(unsigned int)Env0,2.0,512); // thump
+  setup_voice(0, (unsigned int)TriangleTable,200.0,(unsigned int)Env2,1.0,512); // thump
   setup_voice(1,(unsigned int)SquareTable,100.0,(unsigned int)Env1,2.0,200);
   setup_voice(2,(unsigned int)SawTable,200.0,(unsigned int)Env1 ,1.0,512);
-  setup_voice(3,(unsigned int)NoiseTable,800.0,(unsigned int)Env3,.02,500); // snap
+  setup_voice(3,(unsigned int)NoiseTable,800.0,(unsigned int)Env3,.02,512); // snap
+
+  matrix.setBrightness(15);
 
 }
 
@@ -21,83 +29,159 @@ void initSequencer() {
 void pauseSeq() {
   stickClear();
   matrixClear();
+  matrix.fillRect(0,0,3,8,LED_ON);
+  matrix.fillRect(5,0,3,8,LED_ON);
+  matrix.writeDisplay();
+  pauseSynth();
   paused = true;
+  clearBtnStates();
 }
 
 void startSeq() {
+    unpauseSynth();
     paused = false;
+    clearBtnStates();
 }
 
 
 void updateNote() {
   if(newLeft) {
-    int temp = map(left,0,1023,0,63);
+    int temp = map(left,DIAL_MIN_VAL,DIAL_MAX_VAL,0,63);
     if(temp + MIN_NOTE != note) {
       note= temp + MIN_NOTE;
       matrixClear();
       matrix.drawPixel(temp/8,temp%8,LED_ON);
-      matrix.writeDisplay();
+      // matrix.writeDisplay();
+    }
+  }
+  if(newRight) {
+    setup_decay(currentVoice, map(right,DIAL_MIN_VAL,DIAL_MAX_VAL,100,4));
+  }
+}
+
+void updateWave() {
+  int temp;
+  if(newLeft) {
+    temp = map(left,DIAL_MIN_VAL,DIAL_MAX_VAL,1,6);
+    matrixClear();
+    switch(temp) {
+      case 1:
+        matrix.drawBitmap(0, 0, sin_bmp, 8, 8, LED_ON);
+        break;
+      case 2:
+        matrix.drawBitmap(0, 0, tri_bmp, 8, 8, LED_ON);
+        break;
+      case 3:
+        matrix.drawBitmap(0, 0, square_bmp, 8, 8, LED_ON);
+        break;
+      case 4:
+        matrix.drawBitmap(0, 0, saw_bmp, 8, 8, LED_ON);
+        break;
+      case 5:
+        matrix.drawBitmap(0, 0, ramp_bmp, 8, 8, LED_ON);
+        break;
+      case 6:
+        matrix.drawBitmap(0, 0, noise_bmp, 8, 8, LED_ON);
+        break;
+      default:
+        matrix.drawBitmap(0, 0, sin_bmp, 8, 8, LED_ON);
+        break;
+    }
+    setup_wave(currentVoice,temp);
+  }
+  if(newRight) {
+    temp = map(right,DIAL_MIN_VAL,DIAL_MAX_VAL,1,4);
+    matrixClear();
+    switch(temp) {
+      case 1:
+        matrix.drawBitmap(0, 0, env1_bmp, 8, 8, LED_ON);
+        break;
+      case 2:
+        matrix.drawBitmap(0, 0, env2_bmp, 8, 8, LED_ON);
+        break;
+      case 3:
+        matrix.drawBitmap(0, 0, env3_bmp, 8, 8, LED_ON);
+        break;
+      case 4:
+        matrix.drawBitmap(0, 0, env4_bmp, 8, 8, LED_ON);
+        break;
+    }
+    setup_env(currentVoice,temp);
+  }
+}
+
+void resetPattern() {
+  for(int v = 0; v < 4; v++) {
+    for(int b = 0; b < 32; b++) {
+      pattern[v][b]=0;
     }
   }
 }
 
 void sequencer() {
 
-  if(paused) {
-    if(FallBtns&SM_L) {
+  if(FallBtns&SM_L) {
+    if(paused == true) {
       startSeq();
-      clearBtnStates();
+    }
+    else {
+      pauseSeq();
     }
   }
-  else if(synthTick()) {
+  else if(!paused && synthTick()) {
 
     // update values used for synth timing
     bar=counter&0x1f; // loops at 32
 
     updateDials();
 
-    // check for pause button
-    // if(btns[SM_L].fallingEdge()) {
-    if(FallBtns&SM_L) {
-      pauseSeq();
-    }
+    // ---------------------------------------
+    // Handle No Buttons State
+    //
 
     // if no buttons are pressed
     if(!BtnStates) {
       updateNote();
-      if(newRight) {
-        setup_length(currentVoice, map(right,0,1023,24,2));
-      }
     }
+
+    // ---------------------------------------
+    // Handle Small Buttons
+    //
 
     // handle small right: x
     if(BtnStates&SM_R) {
-      if(newLeft) {
-        setup_pitch(1,50+(left/2.0));
+      if(BtnStates == SM_R) {
+        pattern[currentVoice][bar] = 0;
       }
-      if(newRight) {
-        setup_mod(currentVoice, right);
-
+      if(BtnStates == (SM_R + SM_MR)) {
+        resetPattern();
       }
     }
 
     // handle small mid-right: +
     if(BtnStates&SM_MR) {
-      if(newLeft) {
-        setup_wave(1,map(left,0,1023,1,6));
+      if(FallBtns&BIG_L) {
+        currentVoice = 0;
       }
-      if(newRight) {
-        setup_env(currentVoice,map(right,0,1023,1,7));
+      if(FallBtns&BIG_ML) {
+        currentVoice = 1;
       }
+      if(FallBtns&BIG_MR) {
+        currentVoice = 2;
+      }
+      if(FallBtns&BIG_R) {
+        currentVoice = 3;
+      }
+      updateWave();
     }
 
     // handle small mid-left: o
     if(BtnStates&SM_ML) {
       if(newLeft) {
-        setup_wave(1,map(left,0,1023,1,6));
+        setup_tempo(map(left,DIAL_MIN_VAL,DIAL_MAX_VAL,10,60));
       }
       if(newRight) {
-        setup_env(currentVoice,map(right,0,1023,1,7));
+        setup_mod(currentVoice, right);
       }
     }
 
@@ -108,51 +192,19 @@ void sequencer() {
      */
 
     if(FallBtns&BIG_L) {
-      if(btns[SM_ML].read() == LOW) {
-        currentVoice = 0;
-      }
-      else if(btns[SM_R].read() == LOW) {
-        pattern[0][bar] = 0;
-      }
-      else {
-        pattern[0][bar] = note;
-      }
+      pattern[0][bar] = note;
     }
 
     if(FallBtns&BIG_ML) {
-      if(btns[SM_ML].read() == LOW) {
-        currentVoice = 1;
-      }
-      else if(btns[SM_R].read() == LOW) {
-        pattern[1][bar] = 0;
-      }
-      else {
-        pattern[1][bar] = note;
-      }
+      pattern[1][bar] = note;
     }
 
     if(FallBtns&BIG_MR) {
-      if(btns[SM_ML].read() == LOW) {
-        currentVoice = 2;
-      }
-      else if(btns[SM_R].read() == LOW) {
-        pattern[2][bar] = 0;
-      }
-      else {
-        pattern[2][bar] = note;
-      }
+      pattern[2][bar] = note;
     }
 
     if(FallBtns&BIG_R) {
-      if(btns[SM_ML].read() == LOW) {
-        currentVoice = 3;
-      }
-      else if(btns[SM_R].read() == LOW) {
-        pattern[3][bar] = 0;
-      }
-      else {
-        pattern[3][bar] = note;
-      }
+      pattern[3][bar] = note;
     }
 
 
@@ -172,9 +224,14 @@ void sequencer() {
     if(bar/8 != quad) {
       quad = bar/8;
       matrix.clear();
-      matrix.fillRect((quad%2)*4,(quad/2)*4,4,4, LED_ON);
-      matrix.writeDisplay();
+      byte tx = (quad%2)*4;
+      byte ty = (quad/2)*4;
+      matrix.drawPixel(tx+1,ty+1, LED_ON);
+      matrix.drawPixel(tx+2,ty+1, LED_ON);
+      matrix.drawPixel(tx+2,ty+2, LED_ON);
+      matrix.drawPixel(tx+1,ty+2, LED_ON);
     }
+    matrix.writeDisplay();
 
     //************************************************
     // Play Sounds
@@ -190,7 +247,6 @@ void sequencer() {
       mtrigger(2,pattern[2][bar]);
 
     if(pattern[3][bar])
-      // trigger(3);
       mtrigger(3,pattern[3][bar]);
 
     //************************************************
