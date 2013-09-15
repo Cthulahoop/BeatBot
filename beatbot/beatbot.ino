d1894f066093ef40097d3c062b7baad47527da4b
Cthulahoop <cthulhu@formequalsfunction.com>
2013-09-15 15:00:56 -0700

    Updated header comment and added dial init to startup

--- beatbot/beatbot.ino
+++ beatbot/beatbot.ino
@@ -4,7 +4,7 @@
  *   / _ </ -_) _ `/ __/ _ </ _ \/ __/ / _ \/ / / / / / /
  *  /____/\__/\_,_/\__/____/\___/\__/  \___/\__/\__/\__/
  *
- *  v 1.3 08.2013 by Ric Ewing
+ *  v 1.4  15.09.2013 by Ric Ewing
  *  for the MakerHaus Arduino for Kids class
  *
  *    Based on and inspired by:
@@ -15,6 +15,25 @@
  *
  *    - Noise Machine Instructables Mads Høbye
  •      http://www.instructables.com/member/madshobye
+ *
+ *    Known Issues:
+ *    - The size of this sketch is nearing the maximum
+ *      for an Arduino UNO. Some functions can fail or
+ *      behave badly as the compiled skectch size
+ *      increases.
+ *
+ *      For example, I've been able to consistently
+ *      cause the String methods to fail once the
+ *      skectch nears 28k in compiled size.
+ *
+ *      If you modify this code, you may want to seek
+ *      ways to either avoid using the String object
+ *      (http://arduino.cc/en/Reference/StringObject)
+ *      or find ways to optomize the sketch size to
+ *      free up memory to allow them to run).
+ *
+ *    - Enabling the Serial output will cause the
+ *      String methods to fail for the above reasons.
  *
  */
 
@@ -28,10 +47,10 @@ Adafruit_NeoPixel stick = Adafruit_NeoPixel(8, 12, NEO_GRB + NEO_KHZ800);
 Adafruit_8x8matrix matrix = Adafruit_8x8matrix();
 
 #include "vars.h"
+#include "synth.h"
 #include "leds.h"
 #include "buttons.h"
 #include "dials.h"
-#include "synth.h"
 #include "games.h"
 #include "sequencer.h"
 
@@ -39,10 +58,13 @@ byte mode = 1;
 
 void setup() {
 
+  // NOTE: Enabling Serial may cause String methods
+  // to stop working due to lack of memory.
   // Serial.begin(9600);
 
   // setup
   initBtns();
+  initDials();
   initLeds();
   initSynth();
   initSequencer();
